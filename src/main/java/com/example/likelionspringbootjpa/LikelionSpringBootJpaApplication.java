874416e40a9ccd5c26d7160097787247011015d8
mabyoungg <mabyoungg@gmail.com>
2023-11-29 16:41:54 +0900

    init: initial project setup

---
+++ src/main/java/com/example/likelionspringbootjpa/LikelionSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSpringBootJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelionspringbootjpa/LikelionSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionSpringBootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
