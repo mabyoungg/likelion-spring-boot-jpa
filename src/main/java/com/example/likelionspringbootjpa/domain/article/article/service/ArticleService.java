96739708f897948fa18fa0f836cced963452ccbb
mabyoungg <mabyoungg@gmail.com>
2023-12-11 14:41:39 +0900

    feat: 게시물 페이지 속성 반환

--- src/main/java/com/example/likelionspringbootjpa/domain/article/article/service/ArticleService.java
+++ src/main/java/com/example/likelionspringbootjpa/domain/article/article/service/ArticleService.java
@@ -5,6 +5,8 @@ import com.example.likelionspringbootjpa.domain.article.article.repository.Artic
 import com.example.likelionspringbootjpa.domain.member.member.entity.Member;
 import com.example.likelionspringbootjpa.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,5 +46,9 @@ public class ArticleService {
 
     public List<Article> findAll() {
         return articleRepository.findByOrderByIdDesc();
+    }
+
+    public Page<Article> search(Pageable pageable) {
+        return articleRepository.findAll(pageable);
     }
 }
