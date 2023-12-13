5cdaa85e8a733d99b476f451e776ba8a49d386df
mabyoungg <mabyoungg@gmail.com>
2023-12-13 22:14:29 +0900

    refactor: 검색 QueryDSL 적용

---
+++ src/main/java/com/example/likelionspringbootjpa/domain/article/article/repository/ArticleRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.example.likelionspringbootjpa.domain.article.article.repository;
+
+import com.example.likelionspringbootjpa.domain.article.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ArticleRepositoryCustom {
+    Page<Article> search(List<String> kwTypes, String kw, Pageable pageable);
+}
